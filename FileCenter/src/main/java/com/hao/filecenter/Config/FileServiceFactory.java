9ccd1871efe9318fafa520b4e4a3f99b2342b93c
MuggleLee <770796059@qq.com>
2019-07-31 00:05:25 +0800

    Add FileCenter project file

---
+++ FileCenter/src/main/java/com/hao/filecenter/Config/FileServiceFactory.java
@@ -0,0 +1,51 @@
+package com.hao.filecenter.Config;
+
+import com.hao.filecenter.Model.FileSource;
+import com.hao.filecenter.Service.FileService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * FileService工厂<br>
+ * 将各个实现类放入map
+ */
+@Configuration
+public class FileServiceFactory {
+
+    private Map<FileSource, FileService> map = new HashMap<>();
+
+    @Autowired
+    private FileService localFileServiceImpl;
+    @Autowired
+    private FileService aliyunFileServiceImpl;
+
+    @PostConstruct
+    public void init() {
+        map.put(FileSource.LOCAL, localFileServiceImpl);
+        map.put(FileSource.ALIYUN, aliyunFileServiceImpl);
+    }
+
+    /**
+     * 根据文件源获取具体的实现类
+     *
+     * @param fileSource
+     * @return
+     */
+    public FileService getFileService(String fileSource) {
+        if (StringUtils.isBlank(fileSource)) {// 默认用本地存储
+            return localFileServiceImpl;
+        }
+
+        FileService fileService = map.get(FileSource.valueOf(fileSource));
+        if (fileService == null) {
+            throw new IllegalArgumentException("请检查FileServiceFactory类的init方法，看是否有" + fileSource + "对应的实现类");
+        }
+
+        return fileService;
+    }
+}
---
+++ FileCenter/src/main/java/com/hao/filecenter/Model/FileSource.java
@@ -0,0 +1,16 @@
+package com.hao.filecenter.Model;
+
+/**
+ * 文件存储方式
+ */
+public enum FileSource {
+
+    /**
+     * 本地
+     */
+    LOCAL,
+    /**
+     * 阿里云
+     */
+    ALIYUN
+}
