b2d49beb7e07daa75ded68bca0e18a016e09d7e1
MuggleLee <770796059@qq.com>
2019-08-27 17:59:48 +0800

    Modify files

--- CommonUnits/src/main/java/com/hao/commonunits/utils/PhoneUtil.java
+++ CommonUnits/src/main/java/com/hao/commonunits/utils/PhoneUtil.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class PhoneUtil {
 
-	private static String REGEX = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$";
+	private static String REGEX = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0-9]))\\d{8}$";
 	private static Pattern P = Pattern.compile(REGEX);
 
 	/**
