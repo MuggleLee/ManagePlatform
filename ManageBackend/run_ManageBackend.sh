e0a9563b299211b83b6cdc570ba4e24747a74444
MuggleLee <770796059@qq.com>
2019-09-03 17:51:44 +0800

    Add and modify files

---
+++ ManageBackend/run_ManageBackend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Date: 2019-09-03
+#Author: MuggleLee
+#Version: v1.0
+docker ps -a | sed '/^CONTAINER/d' | grep "ManageBackend" | gawk '{cmd="docker rm -f "$1; system(cmd)}'
+docker images | sed '/^IMAGE/d' | grep "manage-backend" | gawk '{cmd="docker rmi "$3; system(cmd)}'
+mvn clean package -P dev docker:build -Dmaven.test.skip;
+docker run -d -p 8091:8091 -e "SPRING_PROFILES_ACTIVE=dev" --name=ManageBackend com.hao/manage-backend:1.0;
